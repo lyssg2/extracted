295559b2b0e49858154858456a8f7350815399ba
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-01-04 19:10:29 -0800

    adding route data

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 
 const htmlRoutes = require('./htmlRoutes')
-const apiRoutes = require('./apiRoutes')
+const coffeeShopRoutes = require('./api/coffeeShopRoutes')
+const reviewRoutes = require('./api/reviewRoutes')
+const userRoutes = require('./api/userRoutes')
 
 router.use('/', htmlRoutes)
-router.use('/api', apiRoutes)
+router.use('/shop', coffeeShopRoutes)
+router.use('/review', reviewRoutes)
+router.use('/login', userRoutes)
 
 module.exports = router
